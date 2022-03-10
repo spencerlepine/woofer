f9f7ce7fa01fb2586f30401805c1e29a201119de
Spencer Lepine <spencerlepine26@gmail.com>
2022-03-09 22:13:51 -0500

    Repository Setup (#1)
    * add whitepaper files
    
    * add API + DB schemas
    
    * add tech stack file
    
    * remove broken prettier
    
    * fix prettier linter error
    
    * modify branch action test report
    
    * add output file for coverage
    
    * update test coverage action
    
    * remove coverage report

---
+++ config/dataKeys.js
@@ -0,0 +1,24 @@
+// Change the document key in ONE place (ex, "_id" or "id")
+// DATA_KEYS_CONSTANTS Object
+const dataKeys = {
+  USER_ID: "id",
+  USER_NAME: "name",
+  USER_ZODIAC: "zodiac_sign",
+  USER_GENDER: "gender",
+  USER_PREFERENCE: "preference",
+  USER_BREED: "breed",
+  USER_BIO: "bio",
+  USER_EMAIL: "email",
+  USER_ZIPCODES: "zipcodes",
+  USER_PICTURES: "pictures",
+  USER_BIRTHYEAR: "birthyear",
+  USER_CHATS: "chats",
+  ZIPCODE: "zipcode",
+  USER_PROFILE: "user_profile",
+  THIS_USER_ID: "this_user_id",
+  THAT_USER_ID: "that_user_id",
+  CHAT_ID: "chat_id",
+  MATCH_STATUS: "status",
+}
+
+export default dataKeys
