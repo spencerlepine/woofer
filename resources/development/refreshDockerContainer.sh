773a09643240a6e702ee2dcb64f87ddda66b6a82
Spencer Lepine <spencerlepine26@gmail.com>
2022-04-08 22:52:23 -0400

    ignore files to shrink docker image size

--- resources/development/refreshDockerContainer.sh
+++ resources/development/refreshDockerContainer.sh
@@ -4,4 +4,4 @@
 
 docker ps -a | grep "woofer" | awk '{print $1}' | xargs docker rm
 docker rmi $(docker images | grep 'woofer')
-docker run -it -p 3000:3000 -t spencerlepine/woofer:latest woofer
+docker run -d -p 3000:3000 -t spencerlepine/woofer:latest woofer
