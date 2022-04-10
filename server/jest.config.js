ebac93de206a6d625ac5f743119ce977ca68b3ca
Spencer Lepine <spencerlepine26@gmail.com>
2022-04-10 18:24:33 -0400

    seperate server packages and config

---
+++ server/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  collectCoverage: true,
+  coverageReporters: ["json", "html", "lcov"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/*.test.js/"],
+  projects: [
+    {
+      displayName: "node",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/**/*.test.js?(x)"],
+      setupFilesAfterEnv: [
+        "<rootDir>/tests/utils/test-setup.js",
+        "<rootDir>/config/jest.setup.js",
+      ],
+      transformIgnorePatterns: ["node_modules/(?!.*?/es/.*\\.js)"],
+    },
+  ],
+  preset: "@shelf/jest-mongodb",
+}
