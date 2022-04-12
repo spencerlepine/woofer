9ff6a98c876e09a7dcb6e32da18e1f060f1bf513
Spencer Lepine <spencerlepine26@gmail.com>
2022-04-12 19:13:59 -0400

    add jest testing config to client

---
+++ client/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  collectCoverage: true,
+  coverageReporters: ["json", "html", "lcov"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/*.test.js/"],
+  projects: [
+    {
+      displayName: "DOM",
+      testEnvironment: "jsdom",
+      transform: {
+        "^.+\\.(js|jsx)$": "babel-jest",
+      },
+      setupFilesAfterEnv: ["<rootDir>/src/utils/test-utils.js"],
+      // "extensionsToTreatAsEsm": [".jsx"],
+      testMatch: ["<rootDir>/**/*.test.js"],
+      setupFiles: ["dotenv/config"],
+      modulePaths: ["/src/"],
+      moduleDirectories: ["node_modules", "src"],
+      watchPlugins: [
+        "jest-watch-typeahead/filename",
+        "jest-watch-typeahead/testname",
+      ],
+      moduleFileExtensions: ["js"],
+      moduleNameMapper: {
+        "\\.(css|less)$": "<rootDir>/src/__mocks__/styleMock.js",
+        "\\.(png|gif|ttf|eot|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
+      },
+      transformIgnorePatterns: ["node_modules/(?!.*?/es/.*\\.js)"],
+    },
+  ],
+}
