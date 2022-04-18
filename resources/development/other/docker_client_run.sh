7e304d9fd3e58f40c36fcc71db09df00fed4e958
Spencer Lepine <spencerlepine26@gmail.com>
2022-04-18 06:14:29 -0400

    add build args for env variables for client Docker image

--- resources/development/other/docker_client_run.sh
+++ resources/development/other/docker_client_run.sh
@@ -1,8 +1,6 @@
 
 # LOAD THE .env into memory
-if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
-fi
+export $(grep -v '^#' .env | xargs)
 
 # Build the client image
 docker build -t client \
@@ -21,4 +19,17 @@ docker build -t client \
 .
 
 # Run the container!
-docker run -it -p 3000:3000 client
+docker run -it -p 3000:3000 --env NODE_ENV=$NODE_ENV \
+--env SERVER_URL=$SERVER_URL \
+--env PORT=$PORT \
+--env MONGODB_URL=$MONGODB_URL \
+--env SKIP_PREFLIGHT_CHECK=$SKIP_PREFLIGHT_CHECK \
+--env REACT_APP_FIREBASE_API_KEY=$REACT_APP_FIREBASE_API_KEY \
+--env REACT_APP_FIREBASE_AUTH_DOMAIN=$REACT_APP_FIREBASE_AUTH_DOMAIN \
+--env REACT_APP_FIREBASE_PROJECT_ID=$REACT_APP_FIREBASE_PROJECT_ID \
+--env REACT_APP_FIREBASE_STORAGE_BUCKET=$REACT_APP_FIREBASE_STORAGE_BUCKET \
+--env REACT_APP_FIREBASE_MESSAGING_SENDER_ID=$REACT_APP_FIREBASE_MESSAGING_SENDER_ID \
+--env REACT_APP_FIREBASE_APP_ID=$REACT_APP_FIREBASE_APP_ID \
+--env REACT_APP_FIREBASE_MEASUREMENT_ID=$REACT_APP_FIREBASE_MEASUREMENT_ID \
+client
+
