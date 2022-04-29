5f4b567074a83aa793a0bb70a374a812aa5cfcc5
Spencer Lepine <spencerlepine26@gmail.com>
2022-04-29 06:08:12 -0400

    fix broken auth context test for ChatPage

--- client/src/components/pages/ChatListPage/ChatList/ChatList.test.js
+++ client/src/components/pages/ChatListPage/ChatList/ChatList.test.js
@@ -1,6 +1,12 @@
 import React from "react"
 import { render, screen } from "utils/test-utils"
-import ChatList from "./ChatList"
+import ChatListCompnent from "./ChatList"
+
+const ChatList = (props) => (
+  <AuthProvider>
+    <ChatListCompnent {...props} />
+  </AuthProvider>
+)
 
 describe("ChatList", () => {
   test("should render without throwing an error", () => {
