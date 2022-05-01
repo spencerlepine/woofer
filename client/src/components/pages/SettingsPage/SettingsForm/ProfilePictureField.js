4dfa3709bd3ff821d68568b835533eb583b9f808
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-01 11:07:55 -0400

    finish building the profile image upload component

--- client/src/components/pages/SettingsPage/SettingsForm/ProfilePictureField.js
+++ client/src/components/pages/SettingsPage/SettingsForm/ProfilePictureField.js
@@ -3,10 +3,10 @@ import { uploadImageToFirebase, deleteImageFromFirebase } from "api/firebase"
 import FormWrapper from "components/ui/AccountFormWrapper/AccountFormWrapper"
 
 import { BiImageAdd } from "react-icons/bi"
+import { MdOutlineRemoveCircleOutline as DeleteIcon } from "react-icons/md"
 
-import constants from "config/constants"
-const { DATA_KEYS } = constants
-const idKey = DATA_KEYS["USER_ID"]
+const missingImage =
+  "https://www.britax-roemer.com/on/demandware.static/Sites-Britax-EU-Site/-/default/dw1be0148e/images/britax/PlaceholderProductImage.jpg"
 
 const ImagesTab = ({
   updateAccountDetails,
@@ -21,22 +21,23 @@ const ImagesTab = ({
   setMadeChange,
 }) => {
   const userId = currentUser[["userId"]]
-  const profileKey = DATA_KEYS["USER_PROFILE_PIC"]
+  const profileKey = "profilePicture"
   const profilePic = accountDetails[profileKey]
 
   const currentProfilePic = profilePic
+  const placeholderProfilePic = currentProfilePic || missingImage
 
   const updateImage = (newImage) => {
     setMadeChange(true)
     const newEntries = {
       ...formEntries,
-      profileKey: newImage,
+      profilePicture: newImage,
     }
     manualSubmit(newEntries)
     // setFormEntries((prevEntries) => {
     //   const newEntries = {
     //     ...prevEntries,
-    //     profileKey: newImage,
+    //     profilePicture: newImage,
     //   }
     //   manualSubmit(newEntries)
 
@@ -48,9 +49,9 @@ const ImagesTab = ({
     e.preventDefault()
     const missing = ""
     const fireRe = new RegExp(/firebase/i)
-    console.log(fireRe.test(imageSrc))
 
-    if (fireRe.test(imageSrc)) {
+    // If it is from firebase, try to delete it
+    if (imageSrc && fireRe.test(imageSrc)) {
       deleteImageFromFirebase(imageSrc, userId, () => {
         updateImage(missing)
       })
@@ -59,63 +60,73 @@ const ImagesTab = ({
     }
   }
 
+  const RemoveBtn = () => (
+    <>
+      {currentProfilePic && (
+        <button
+          className="button is-danger is-medium p-2"
+          onClick={(e) => handleDelete(e, currentProfilePic)}
+        >
+          Remove <DeleteIcon />
+        </button>
+      )}
+    </>
+  )
+
+  const UploadImageBtn = () => (
+    <div className="file is-boxed is-centered">
+      <label className="file-label">
+        <input
+          className="file-input"
+          type="file"
+          accept="image/*"
+          name="picture"
+          onChange={(event) => {
+            const imageFile = event.target.files[0]
+            if (imageFile.size < 2097152) {
+              setMadeChange(true)
+              uploadImageToFirebase(imageFile, userId, updateImage)
+            }
+          }}
+          onClick={(event) => {
+            event.target.value = null
+          }}
+        />
+        <span className="button file-cta is-primary">
+          <span className="file-icon is-inline">
+            <BiImageAdd className="icon is-large" />
+          </span>
+          <span className="file-label is-inline">Upload Image</span>
+        </span>
+      </label>
+    </div>
+  )
+
   return (
     <>
-      <div className="tile is-ancestor">
-        <div className="tile">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-128x128">
-                <img src={currentProfilePic} alt="User Thumbnail" />
-              </figure>
-            </div>
+      <div className="columns">
+        <div className="column is-half">
+          <figure className="image is-128x128 profilePic">
+            <img src={placeholderProfilePic} alt="User Thumbnail" />
+          </figure>
+        </div>
 
-            {currentProfilePic && (
-              <button
-                className="delete is-medium p-2"
-                onClick={(e) => handleDelete(e, currentProfilePic)}
-              ></button>
-            )}
-          </div>
+        <div className="column is-half">
+          <RemoveBtn />
+          <UploadImageBtn />
         </div>
       </div>
-
-      {currentProfilePic && (
-        <div className="tile">
-          <div className="file is-boxed">
-            <label className="file-label">
-              <input
-                className="file-input"
-                type="file"
-                accept="image/*"
-                name="picture"
-                onChange={(event) => {
-                  const imageFile = event.target.files[0]
-                  if (imageFile.size < 2097152) {
-                    setMadeChange(true)
-                    uploadImageToFirebase(imageFile, userId, handleImageAdd)
-                  }
-                }}
-                onClick={(event) => {
-                  event.target.value = null
-                }}
-              />
-              <span className="button file-cta is-primary">
-                <span className="file-icon">
-                  <BiImageAdd className="icon is-large" />
-                </span>
-                <span className="file-label">Upload Picture…</span>
-              </span>
-            </label>
-          </div>
-        </div>
-      )}
     </>
   )
 }
 
 const WrappedTab = (props) => (
-  <FormWrapper FieldsComponent={ImagesTab} LargeWidth={true} {...props} />
+  <FormWrapper
+    FieldsComponent={ImagesTab}
+    LargeWidth={true}
+    {...props}
+    hideButtons
+  />
 )
 
 export default WrappedTab
--- client/src/components/pages/SettingsPage/SettingsPage.js
+++ client/src/components/pages/SettingsPage/SettingsPage.js
@@ -12,6 +12,8 @@ const SettingsPage = () => {
 
       <ProfilePictureField />
 
+      <br />
+
       <SettingsForm />
 
       <EmailReset />
