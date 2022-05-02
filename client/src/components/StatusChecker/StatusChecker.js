24c821c79fc6544089037fcdb668140c7026936c
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-02 17:52:01 -0400

    render clean loading spinner for StatusCheck

--- client/src/components/StatusChecker/StatusChecker.js
+++ client/src/components/StatusChecker/StatusChecker.js
@@ -3,6 +3,7 @@ import { IoWarning as WarningIcon } from "react-icons/io5"
 import { BiSupport as SupportIcon } from "react-icons/bi"
 import WooferLogo from "assets/WooferLogo.png"
 import Socials from "config/socialConstants"
+import Footer from "components/ui/Footer/Footer"
 
 const StatusCheck = (props) => {
   const [loading, setLoading] = useState(true)
@@ -31,23 +32,23 @@ const StatusCheck = (props) => {
     }
   }, [firebaseCheckDone, serverCheckDone])
 
-  useEffect(() => {
-    if (serverCheckDone === false) {
-      checkServerStatus((statusCheckPassed) => {
-        setServerConnectionIsValid(statusCheckPassed)
-        setServerCheckDone(true)
-      })
-    }
-  }, [serverCheckDone])
+  // useEffect(() => {
+  //   if (serverCheckDone === false) {
+  //     checkServerStatus((statusCheckPassed) => {
+  //       setServerConnectionIsValid(statusCheckPassed)
+  //       setServerCheckDone(true)
+  //     })
+  //   }
+  // }, [serverCheckDone])
 
-  useEffect(() => {
-    if (firebaseCheckDone === false) {
-      checkFirebaseConfig((statusCheckPassed) => {
-        setFirebaseConfigIsValid(statusCheckPassed)
-        setFirebaseCheckDone(true)
-      })
-    }
-  }, [firebaseCheckDone])
+  // useEffect(() => {
+  //   if (firebaseCheckDone === false) {
+  //     checkFirebaseConfig((statusCheckPassed) => {
+  //       setFirebaseConfigIsValid(statusCheckPassed)
+  //       setFirebaseCheckDone(true)
+  //     })
+  //   }
+  // }, [firebaseCheckDone])
 
   const InfoOrAlert = () => (
     <>
@@ -55,10 +56,10 @@ const StatusCheck = (props) => {
         <>{props.children}</>
       ) : (
         <>
-          <section class="section">
-            <div class="container">
-              <div class="columns is-centered">
-                <div class="column is-half">
+          <section className="section">
+            <div className="container">
+              <div className="columns is-centered">
+                <div className="column is-half">
                   <div className="field has-text-centered">
                     <img
                       className=" has-text-centered"
@@ -68,12 +69,12 @@ const StatusCheck = (props) => {
                     ></img>
                   </div>
 
-                  <article class="message is-warning">
-                    <div class="message-header">
+                  <article className="message is-warning">
+                    <div className="message-header">
                       <p>Contact Support</p>
                       <SupportIcon />
                     </div>
-                    <div class="message-body">
+                    <div className="message-body">
                       Please reach out to{" "}
                       <span className="has-text-link">
                         <a
@@ -89,21 +90,23 @@ const StatusCheck = (props) => {
                   </article>
 
                   {!firebaseConfigIsValid && (
-                    <article class="message is-danger">
-                      <div class="message-header">
+                    <article className="message is-danger">
+                      <div className="message-header">
                         <p>Error</p>
                         <WarningIcon />
                       </div>
-                      <div class="message-body">Unable to connect to Firebase</div>
+                      <div className="message-body">
+                        Unable to connect to Firebase
+                      </div>
                     </article>
                   )}
                   {!serverConnectionIsValid && (
-                    <article class="message is-danger">
-                      <div class="message-header">
+                    <article className="message is-danger">
+                      <div className="message-header">
                         <p>Server Failure</p>
                         <WarningIcon />
                       </div>
-                      <div class="message-body">
+                      <div className="message-body">
                         Backend API is not currently running
                       </div>
                     </article>
@@ -112,39 +115,39 @@ const StatusCheck = (props) => {
               </div>
             </div>
           </section>
-          <footer className="footer">
-            <div className="columns pr-4 pr-4">
-              <div className="column has-text-left">
-                <p>Built by Spencer Lepine</p>
-
-                <p>
-                  view{" "}
-                  <a href="https://github.com/spencerlepine/woofer">Source Code</a>
-                </p>
-              </div>
-
-              <div className="column has-text-right">
-                <h4>Find me here</h4>
-                {Socials.map((social, i) => (
-                  <a
-                    href={social.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    key={i}
-                    className="socialLink"
-                  >
-                    <p>{social.name}</p>
-                  </a>
-                ))}
-              </div>
-            </div>
-          </footer>
+          <Footer />
         </>
       )}
     </>
   )
 
-  return <>{loading ? <p>Loading...</p> : <InfoOrAlert />}</>
+  const LoadingSpinner = () => (
+    <>
+      <div className="section is-vcentered">
+        <div className="columns  is-flex is-vcentered">
+          <div className="column is-6 mx-auto">
+            <h2 className="title is-2">
+              <div className="loader-wrapper mx-auto">
+                <div className="loader is-loading mx-auto"></div>
+              </div>
+
+              <div className="field has-text-centered mt-6">
+                <img
+                  className=" has-text-centered"
+                  width="167"
+                  src={WooferLogo}
+                  alt="Woofer Logo"
+                ></img>
+              </div>
+            </h2>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+
+  return <>{loading ? <LoadingSpinner /> : <InfoOrAlert />}</>
 }
 
 export default StatusCheck
