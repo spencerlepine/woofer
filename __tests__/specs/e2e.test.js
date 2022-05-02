9d975c89fe07851c63e91c87b4120489b88e3f95
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-02 18:23:57 -0400

    add end-to-end tests for working component renders

--- __tests__/specs/e2e.test.js
+++ __tests__/specs/e2e.test.js
@@ -14,5 +14,31 @@ describe("Woofer Homepage", () => {
     expect(true).toBeTruthy()
   })
 
+  it("should render React App component", async () => {
+    await page.waitForSelector(".App")
+    // const text = await page.$eval(".App", (e) => e.textContent);
+    const AppComponent = await page.$eval(".App", (e) => e.innerHTML)
+    expect(AppComponent).toBeTruthy()
+  })
+
+  it("should render React App component", async () => {
+    await page.waitForSelector(".App")
+    // const text = await page.$eval(".App", (e) => e.textContent);
+    const AppComponent = await page.$eval(".App", (e) => e.innerHTML)
+    expect(AppComponent).toBeTruthy()
+  })
+
+  it("should render navbar", async () => {
+    await page.waitForSelector(".navbar")
+    const AppComponent = await page.$eval(".navbar", (e) => e.innerHTML)
+    expect(AppComponent).toBeTruthy()
+  })
+
+  it("should render footer", async () => {
+    await page.waitForSelector(".footer")
+    const AppComponent = await page.$eval(".footer", (e) => e.innerHTML)
+    expect(AppComponent).toBeTruthy()
+  })
+
   afterAll(() => browser.close())
 })
