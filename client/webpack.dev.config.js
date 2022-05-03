cea837f89b6d3bfed2c44471de8f6e3033beb921
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-03 17:25:40 -0400

    remove CORS header from end-to-end testing webpack config

--- client/webpack.dev.config.js
+++ client/webpack.dev.config.js
@@ -43,12 +43,12 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     historyApiFallback: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
-    },
+    // headers: {
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    //   "Access-Control-Allow-Headers":
+    //     "X-Requested-With, content-type, Authorization",
+    // },
   },
   module: {
     rules: [
