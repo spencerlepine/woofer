a8f442cf66517fb7bc82d7c9ebc7696edfcf8f0a
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-03 17:07:44 -0400

    add CORS header to WEBPACK config

--- client/webpack.prod.config.js
+++ client/webpack.prod.config.js
@@ -49,6 +49,12 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
   },
   plugins: [
     new CompressionPlugin({
