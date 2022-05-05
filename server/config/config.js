bff44d87a2bf087f1818159c68984cd6efd38303
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-04 20:33:37 -0400

    revert .dotenv

--- server/config/config.js
+++ server/config/config.js
@@ -5,38 +5,40 @@
  */
 
 const path = require("path")
-// const Joi = require("joi")
+const Joi = require("joi")
 
-// let config = {}
+let config = {}
 
 if (process.env.NODE_ENV !== "production") {
   const dotenv = require("dotenv")
   dotenv.config({ path: path.join(__dirname, "../../.env") })
+} else {
+  const dotenv = require("dotenv")
+  dotenv.config({ path: path.join(__dirname, "../.env") })
 }
 
-// const envVarsSchema = Joi.object()
-//   .keys({
-//     NODE_ENV: Joi.string()
-//       .valid("production", "development", "test")
-//       .default("production"),
-//     PORT: Joi.string().required().description("Entry port the express server"),
-//     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
-//   })
-//   .unknown()
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .default("production"),
+    PORT: Joi.string().required().description("Entry port the express server"),
+    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+  })
+  .unknown()
 
-// const { value: envVars, error } = envVarsSchema
-//   .prefs({ errors: { label: "key" } })
-//   .validate(process.env)
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env)
 
-// if (error) {
-//   throw new Error(`Config validation error: ${error.message}`)
-// }
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
 
 module.exports = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
+  ...envVars,
   MONGOOSE: {
-    url: process.env.MONGODB_URL,
+    url: envVars.MONGODB_URL,
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
