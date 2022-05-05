0a1e629d4459604d3bfa315cd928d14e7f248afe
Spencer Lepine <spencerlepine26@gmail.com>
2022-05-04 20:44:23 -0400

    update config settings

--- server/config/config.js
+++ server/config/config.js
@@ -7,38 +7,16 @@
 const path = require("path")
 const Joi = require("joi")
 
-let config = {}
-
 if (process.env.NODE_ENV !== "production") {
   const dotenv = require("dotenv")
   dotenv.config({ path: path.join(__dirname, "../../.env") })
-} else {
-  const dotenv = require("dotenv")
-  dotenv.config({ path: path.join(__dirname, "../.env") })
-}
-
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string()
-      .valid("production", "development", "test")
-      .default("production"),
-    PORT: Joi.string().required().description("Entry port the express server"),
-    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
-  })
-  .unknown()
-
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
-  .validate(process.env)
-
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`)
 }
 
 module.exports = {
-  ...envVars,
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT || 5000,
   MONGOOSE: {
-    url: envVars.MONGODB_URL,
+    url: process.env.MONGODB_URL,
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -47,3 +25,21 @@ module.exports = {
   // SERVER_URL: `http://localhost:${envVars.PORT}`,
   SERVER_URL: "http://localhost",
 }
+
+// const envVarsSchema = Joi.object()
+//   .keys({
+//     NODE_ENV: Joi.string()
+//       .valid("production", "development", "test")
+//       .default("production"),
+//     PORT: Joi.string().required().description("Entry port the express server"),
+//     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+//   })
+//   .unknown()
+
+// const { value: envVars, error } = envVarsSchema
+//   .prefs({ errors: { label: "key" } })
+//   .validate(process.env)
+
+// if (error) {
+//   throw new Error(`Config validation error: ${error.message}`)
+// }
